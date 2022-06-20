f5e6fe9c9cc8bebfd2743c7d285aeb22bee637bf
Alexander Wulf <aw@dc.ag>
2022-06-20 20:14:34 +0200

    Xdebug support for Artisan console

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,6 +7,22 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+
+    public function bootstrap()
+    {
+        // Don't forget to call parent bootstrap
+        parent::bootstrap();
+
+        if(config('app.debug')){
+            try {
+                // @phpstan-ignore-next-line
+                xdebug_connect_to_client();
+            } catch (\Throwable $t){
+                print_r("Even though debug mode is enabled it seems that you don't have XDebug installed\n\n");
+            }
+        }
+    }
+
     /**
      * Define the application's command schedule.
      *
